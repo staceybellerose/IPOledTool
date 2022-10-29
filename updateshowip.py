51d7a79047777c993535146d2d9f76d31567a6a7
Stacey Adams <stacey.belle.rose@gmail.com>
2022-10-29 09:42:08 -0500

    Refactor code and clean up Pylint warnings (#1)

--- updateshowip.py
+++ updateshowip.py
@@ -1,100 +1,83 @@
 #!/usr/bin/python3
 
-#
-# J.A. Korten
-# July 7, 2021
-# Script to show hostname, eth0 and wlan0 addresses on small oled
-#
-# Don't forget to sudo chmod +x updateshowip.py on the script and add it as a cronjob
-#
-# we will run it as systemd process to avoid startup issues
-# see also: http://iltabiai.github.io/raspberry%20pi/python/linux/ubuntu/telegram/2020/05/08/python-systemd.html
+"""
+Author: J.A. Korten, July 7, 2021
+Modified: Stacey Adams, October 29, 2022
 
-import schedule
+Script to show hostname, eth0 and wlan0 addresses on small oled.
+
+Don't forget to sudo chmod +x updateshowip.py on the script and add it as a cronjob.
+
+We will run it as systemd process to avoid startup issues; see also:
+http://iltabiai.github.io/raspberry%20pi/python/linux/ubuntu/telegram/2020/05/08/python-systemd.html
+"""
+
 import time
-import schedule
-import time
-import subprocess
-import sys
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "reinstall", package])
-
-from board import SCL, SDA
-'''
-ToDo: fix auto installing blinka.
-except:
-    # adafruit-blinka
-    print("board package not found, trying to install adafruit-blinka")
-    install("adafruit-blinka")
-    try:
-        from board import SCL, SDA
-    except:
-        print("Still an issue with board package. Aborting.")
-'''
-print("IPOledTool script was executed")
-print("This script prints hostname, eth0 and wlan0 addresses on small oled")
-print("Values are update every second")
-print("J.A. Korten - 2021")
-print()
-print("Initial values:")
-
-
-import busio
-from oled_text import OledText
-from more_scripts import getip
-import socket
-
-try:
-    i2c = busio.I2C(SCL, SDA)
-except:
-    print("I2C error, maybe you forgot to enable it through raspi-config?")
-    exit()
-# Create the display, pass its pixel dimensions
-oled = OledText(i2c, 128, 32)
-
-# Write to the oled
-
-def showIPOnOled():
-     eth0ip = get_ip_address('eth0')
-     wlan0ip = get_ip_address('wlan0')
-     hostname = socket.gethostname()
-     oled.text("H: " + hostname, 1)
-     oled.text("E: " + eth0ip, 2)
-     oled.text("W: " + wlan0ip, 3)
-     #print("Update...") # shows that values are indeed updated...
-
-def printInitialValues():
-     eth0ip = getip.get_ip_address('eth0')
-     wlan0ip = getip.get_ip_address('wlan0')
-     hostname = socket.gethostname()
-     print("Hostname: " + hostname)
-     print("Ethernet: " + eth0ip)
-     print("Wireless: " + wlan0ip)
-
-printInitialValues()
-print()
-
 import socket
 import fcntl
 import struct
+import schedule
+import busio
+from board import SCL, SDA
+from oled_text import OledText
 
+print("IPOledTool script was executed")
+print("This script prints hostname, eth0 and wlan0 addresses on small oled")
+print("Values are updated every second")
+print("J.A. Korten - 2021; Stacey Adams - 2022")
+print()
 
+try:
+    I2C = busio.I2C(SCL, SDA)
+except RuntimeError:
+    print("I2C error, maybe you forgot to enable it through raspi-config?")
+    exit()
+
+# Create the display, pass its pixel dimensions
+OLED = OledText(I2C, 128, 32)
+
+SIOCGIFADDR = 0x8915
 
 def get_ip_address(ifname):
+    """Get the IP address of the provided interface."""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ifreq = struct.pack('256s', bytes(ifname[:15], 'utf-8'))
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(s.fileno(),
-                0x8915,  # SIOCGIFADDR
-                struct.pack('256s', bytes(ifname[:15], 'utf-8')))[20:24])
-    except:
-        return "n/a"
+        res = fcntl.ioctl(sock.fileno(), SIOCGIFADDR, ifreq)
+    except OSError:
+        return 'n/a'
+    return socket.inet_ntoa(res[20:24])
 
-def perform_task():
-    showIPOnOled()
+# Collect the data to display
+def collect_data():
+    """Collect the IP addresses and host name."""
+    eth0ip = get_ip_address('eth0')
+    wlan0ip = get_ip_address('wlan0')
+    hostname = socket.gethostname()
+    return (eth0ip, wlan0ip, hostname)
 
-schedule.every(1).seconds.do(perform_task)
+# Write to the oled
+def show_ip_on_oled():
+    """Write the IP addresses and host name to the OLED."""
+    (eth0ip, wlan0ip, hostname) = collect_data()
+    OLED.text("H: " + hostname, 1)
+    OLED.text("E: " + eth0ip, 2)
+    OLED.text("W: " + wlan0ip, 3)
+    #print "Update..." # shows that values are indeed updated...
 
-while (1):
+def print_initial_values():
+    """Print the initial values of the IP addresses and host name."""
+    (eth0ip, wlan0ip, hostname) = collect_data()
+    print("Initial values:")
+    print("Hostname: " + hostname)
+    print("Ethernet: " + eth0ip)
+    print("Wireless: " + wlan0ip)
+
+print_initial_values()
+print()
+
+schedule.every(1).seconds.do(show_ip_on_oled)
+
+while True:
     schedule.run_pending()
     time.sleep(1)
